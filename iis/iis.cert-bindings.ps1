bfefed546ba2c56fdefff21b45a5d8a79417f7f8
Xzenor <michele@possamai.nl>
2019-08-09 12:25:24 +0200

    added

---
+++ iis/iis.cert-bindings.ps1
@@ -0,0 +1,37 @@
+import-module WebAdministration
+
+Function ListCertBindings
+{
+	Get-ChildItem cert:\LocalMachine\my | Select-Object * | Sort-Object -Property FriendlyName | ForEach-Object  {
+		$found = $false
+		$tp = $_.Thumbprint
+		Write-Host "***************************************************************"
+		Get-ChildItem IIS:\SslBindings | ForEach-Object {
+			if ($_.Thumbprint -eq $tp)
+			{
+			Write-Host "Used in $($_.IpAddress) $($_.Host)" #-foregroundcolor green
+			$found = $true
+			} 
+		}
+		if ($found)
+		{
+			Write-Host "FriendlyName =" $_.FriendlyName -foregroundcolor yellow
+			Write-Host $tp -foregroundcolor green
+			Write-Host $_.Subject -foregroundcolor green
+			Write-Host $_.NotAfter -foregroundcolor green
+		}
+		else
+		{
+			Write-Host "FriendlyName =" $_.FriendlyName -foregroundcolor yellow
+			Write-Host "Not in use"
+			Write-Host $tp -foregroundcolor red
+			Write-Host $_.Subject -foregroundcolor red
+			Write-Host $_.NotAfter -foregroundcolor red
+		}
+		Write-Host "***************************************************************"
+		Write-Host
+	}
+}
+
+ListCertBindings
+
